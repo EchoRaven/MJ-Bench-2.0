e4b2e9ad0e2efa2c2f85c4bfc957c37f57061173
EchoRaven <thb20021120@126.com>
2024-09-08 06:06:21 +0800

    use sft as main frame

--- videoRM/swift/swift_finetune.sh
+++ videoRM/swift/swift_finetune.sh
@@ -10,7 +10,7 @@ swift sft \
     --sft_type lora \
     --tuner_backend peft \
     --dtype AUTO \
-    --model_id_or_path /remote_shome/snl/feilong/xiapeng/haibo/videoRM/InternVL-main/pretrained/InternVL2-2B \
+    --model_id_or_path /remote_shome/snl/feilong/xiapeng/haibo/videoRM/Internvl/pretrained/InternVL2-2B \
     --custom_train_dataset_path /remote_shome/snl/feilong/xiapeng/haibo/videoRM/dataset/SafeSora_json/swift_train.jsonl \
     --custom_val_dataset_path /remote_shome/snl/feilong/xiapeng/haibo/videoRM/dataset/SafeSora_json/swift_test.jsonl \
     --learning_rate 4e-5 \
@@ -21,12 +21,13 @@ swift sft \
     --save_steps 200 \
     --save_total_limit 1 \
     --learning_rate 4e-5 \
+    --deepspeed "zero_stage3_config.json" \
     --num_train_epochs 1 \
     --save_steps 200 \
     --train_dataset_sample -1 \
     --logging_steps 1 \
     --batch_size 1 \
-    --lora_rank 2 \
+    --lora_rank 16 \
     --lora_alpha 32 \
     --max_grad_norm 0.5 \
     --lora_dropout 0.05 \
