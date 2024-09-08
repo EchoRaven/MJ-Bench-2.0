b201d1df59d35ec1e193d4ef11c239d59fdfd0c9
EchoRaven <thb20021120@126.com>
2024-09-08 16:46:43 +0800

    add new sh file

--- videoRM/swift/swift_finetune.sh
+++ videoRM/swift/swift_finetune.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Set the CUDA devices
-export CUDA_VISIBLE_DEVICES=1,5
+export CUDA_VISIBLE_DEVICES=6,7
 
-# Run the swift sft command with the provided arguments
-swift sft \
+# Run the swift sft command with nohup and output logs to a file
+nohup swift sft \
     --model_type internvl2-2b \
     --model_revision master \
     --sft_type lora \
     --tuner_backend peft \
-    --dtype AUTO \
+    --dtype bf16 \
     --model_id_or_path /remote_shome/snl/feilong/xiapeng/haibo/videoRM/Internvl/pretrained/InternVL2-2B \
     --custom_train_dataset_path /remote_shome/snl/feilong/xiapeng/haibo/videoRM/dataset/SafeSora_json/swift_train.jsonl \
     --custom_val_dataset_path /remote_shome/snl/feilong/xiapeng/haibo/videoRM/dataset/SafeSora_json/swift_test.jsonl \
@@ -20,17 +20,20 @@ swift sft \
     --save_strategy "steps" \
     --save_steps 200 \
     --save_total_limit 1 \
+    --gradient_checkpointing true \
     --learning_rate 4e-5 \
-    --deepspeed "/remote_shome/snl/feilong/xiapeng/haibo/videoRM/deepspeed_config/zero_stage3_config.json" \
     --num_train_epochs 1 \
     --save_steps 200 \
     --train_dataset_sample -1 \
     --logging_steps 1 \
     --batch_size 1 \
-    --lora_rank 16 \
+    --lora_rank 4 \
     --lora_alpha 32 \
     --max_grad_norm 0.5 \
     --lora_dropout 0.05 \
     --weight_decay 0.01 \
     --warmup_ratio 0.03 \
-    --lr_scheduler_type "cosine"
+    --lr_scheduler_type "cosine" \
+    --lazy_tokenize true \
+    --preprocess_num_proc 4 \
+    --use_flash_attn true > /remote_shome/snl/feilong/xiapeng/haibo/videoRM/logs/finetune.log 2>&1 &
