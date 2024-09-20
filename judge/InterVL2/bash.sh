d98804c604d8365a30b7d2e7404e5ee100e037ab
EchoRaven <thb20021120@126.com>
2024-09-21 00:03:34 +0800

    add some file

--- judge/InterVL2/bash.sh
+++ judge/InterVL2/bash.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-python evaluate_videos.py \
-    --json_file_path "/remote_shome/snl/feilong/xiapeng/haibo/videoRM/dataset/SafeSora/config-train.json" \
-    --videos_dir "/remote_shome/snl/feilong/xiapeng/haibo/videoRM/dataset/SafeSora" \
+python label.py \
+    --json_file_path "../../videoRM/dataset/SafeSora/config-train.json" \
+    --videos_dir "../../videoRM/dataset/SafeSora" \
     --model_path "OpenGVLab/InternVL2-8B" \
-    --cache_dir "/remote_shome/snl/feilong/xiapeng/haibo/videoRM/Internvl/pretrained" \
-    --alignment_results_file "alignment_result.json" \
-    --safety_results_file "safety_result.json" \
-    --bias_results_file "bias_result.json" \
-    --quality_results_file "quality_result.json" \
-    --cc_results_file "cc_result.json"
+    --cache_dir "../../videoRM/Internvl/pretrained" \
+    --alignment_results_file "./result/alignment_result.json" \
+    --safety_results_file "./result/safety_result.json" \
+    --bias_results_file "./result/bias_result.json" \
+    --quality_results_file "./result/quality_result.json" \
+    --cc_results_file "./result/cc_result.json"
