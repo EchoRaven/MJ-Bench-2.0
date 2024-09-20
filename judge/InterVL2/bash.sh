d197a1d630e767bd7fd058691c47b8248fdeb0a9
EchoRaven <thb20021120@126.com>
2024-09-21 00:13:04 +0800

    add some rubbish file

--- judge/InterVL2/bash.sh
+++ judge/InterVL2/bash.sh
@@ -3,7 +3,7 @@
 python label.py \
     --json_file_path "../../videoRM/dataset/SafeSora/config-train.json" \
     --videos_dir "../../videoRM/dataset/SafeSora" \
-    --model_path "OpenGVLab/InternVL2-8B" \
+    --model_path "OpenGVLab/InternVL2-26B" \
     --cache_dir "../../videoRM/Internvl/pretrained" \
     --alignment_results_file "./result/alignment_result.json" \
     --safety_results_file "./result/safety_result.json" \
