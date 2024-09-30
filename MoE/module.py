39c49b4e334e188c4ae657302a8b21612db856fb
EchoRaven <thb20021120@126.com>
2024-09-30 23:06:39 +0800

    add

--- MoE/module.py
+++ MoE/module.py
@@ -25,7 +25,6 @@ class MJ_VIDEO:
     def __init__(self, config):
         self.config = config
         self.dtype = torch.bfloat16 if config["dtype"] == "bfloat16" else torch.float32
-        self.model.generation_config.max_new_tokens = 1024
         template_type = get_default_template_type(config["model_type"])
         self.template = get_template(template_type, self.tokenizer)
         seed_everything(42)
@@ -40,18 +39,20 @@ class MJ_VIDEO:
             # define router
             self.router, _ = get_model_tokenizer(config["router_path"], self.dtype,
                                        model_kwargs={'device_map': 'auto'})
-
+            self.router.generation_config.max_new_tokens = 1024
             # define experts
             self.expert_group = {}
             for key in config["experts"].keys():
                 self.expert_group[key], _ = get_model_tokenizer(config["experts"][key], self.dtype,
                                        model_kwargs={'device_map': 'auto'})
+                self.self.expert_group[key].generation_config.max_new_tokens = 1024
 
     def router_choice(self, video_paths, prompt):
         router_template = self.prompt_list["router"]
         if self.config["inference_type"] == "low_cost":
             self.router, _ = get_model_tokenizer(config["router_path"], self.dtype,
                                        model_kwargs={'device_map': 'auto'})
+            self.router.generation_config.max_new_tokens = 1024
         response, _ = inference(self.router, self.template, router_template + prompt, videos=video_paths)  # chat with image
         return response
 
