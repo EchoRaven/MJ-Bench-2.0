e7256c0fe81717bae582fc52024d0c944f49e694
EchoRaven <thb20021120@126.com>
2024-10-07 14:05:31 +0800

    add

--- eval/eval_1_gemini_thread.py
+++ eval/eval_1_gemini_thread.py
@@ -251,7 +251,6 @@ def process_json_file(json_file_path, videos_dir, output_file_name, key, model_i
 if __name__ == "__main__": 
     videos_dir = '../videos'
     json_files = {
-        'overall': '../test/overall.json',
         'safety': '../test/safety.json',
         'alignment': '../test/alignment.json',
         'bias': '../test/bias.json',
