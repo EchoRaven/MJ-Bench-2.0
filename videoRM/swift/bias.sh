bf90691771e5d62bd5c1e4ee857c6b41c6973813
EchoRaven <thb20021120@126.com>
2024-10-09 16:06:27 +0800

    a

--- videoRM/swift/bias.sh
+++ videoRM/swift/bias.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
+#SBATCH --job-name=moe
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-node=2
+#SBATCH --cpus-per-gpu=28
+#SBATCH --mem-per-cpu=9200
+#SBATCH --output=slurm_output_%j.out
+#SBATCH --error=slurm_error_%j.err
 
-export CUDA_VISIBLE_DEVICES=0,6
-
-# Run the swift sft command with nohup and output logs to a file
-nohup swift sft \
+swift sft \
     --model_type internvl2-4b \
     --model_revision master \
     --sft_type lora \
     --dtype bf16 \
-    --model_id_or_path /remote_shome/snl/feilong/xiapeng/haibo/videoRM/Internvl/pretrained/InternVL2-4B \
-    --custom_train_dataset_path ../videoRM/dataset/swift_json_file/swift_cc_train.jsonl \
-    --custom_val_dataset_path ../videoRM/dataset/swift_json_file/swift_cc_test.jsonl \
+    --model_id_or_path ../Internvl/pretrain/InternVL2-4B \
+    --custom_train_dataset_path ../dataset/swift_json_file/swift_alignment_train.jsonl \
+    --custom_val_dataset_path ../dataset/swift_json_file/swift_alignment_test.jsonl \
     --learning_rate 4e-5 \
-    --output_dir ../videoRM/finetune_result/cc_expert_Internvl2_4B_base_model_lora \
+    --output_dir ../finetune_result/alignment_expert_Internvl2_4B_base_model_lora \
     --max_length 4096 \
     --evaluation_strategy "steps" \
     --eval_steps 100 \
@@ -35,4 +41,4 @@ nohup swift sft \
     --lr_scheduler_type "cosine" \
     --lazy_tokenize true \
     --preprocess_num_proc 4 \
-    --use_flash_attn true > swift_Internvl2_4B_cc_sft_output.log 2>&1 &
+    --use_flash_attn true
