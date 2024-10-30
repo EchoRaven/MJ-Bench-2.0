4c8c38ff0ac3bbb907b4a7ccc3cd0c3fe3574341
EchoRaven <thb20021120@126.com>
2024-10-31 03:30:47 +0800

    add

--- MoE/module.py
+++ MoE/module.py
@@ -377,13 +377,12 @@ class MJ_VIDEO:
 
 
 if __name__ == "__main__":
-    with open("MoE_config_2B_short_sample.json", "r", encoding="utf-8") as f:
+    with open("MoE_config_2B_short_analysis_sample.json", "r", encoding="utf-8") as f:
         config = json.load(f)
     model = MJ_VIDEO(config)
     prompt_type = "single_video_analysis_prompt_template"
-    video_paths = ["../videos/hpdbv2_videos/HPDv2_train_image0_5941.mp4", "../videos/hpdbv2_videos/HPDv2_train_image1_5941.mp4"]
     prompt = "In the video, we observe a peaceful landscape painted with a blend of blue and yellow shades showcasing stylized green trees with varying leaf shapes, such as circular and teardrop leaves. Yellow and green foliage dominates the scenery, interspersed with dark tree trunks. The background sky blends with the foreground seamlessly. The scene is tranquil, with no apparent movement of elements such as people or animals. The painting has a surreal, dream-like quality that evokes a sense of calm and wonder."
-    force_keys = ["bias_fairness"]
+    force_keys = ["alignment", "safety", "quality", "coherence_consistency", "bias_fairness"]
     result = model.inference(["../videos/hpdbv2_videos/HPDv2_train_image0_5941.mp4"], prompt, prompt_type, force_keys)
     print(result)
     result = model.inference(["../videos/hpdbv2_videos/HPDv2_train_image1_5941.mp4"], prompt, prompt_type, force_keys)
